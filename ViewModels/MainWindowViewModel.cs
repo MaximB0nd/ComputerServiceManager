0f8930976efed8615f177241d65029a835627523
MaximB0nd <sersim11@yandex.ru>
2025-09-26 09:56:58 +0300

    proj: init project

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace ComputerService.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ComputerService.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
