65571ab0866d58f7e2fdb358aa0a1ef939c1422b
MaximB0nd <sersim11@yandex.ru>
2025-10-02 10:38:23 +0300

    feat: added base navigation

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,22 @@
-﻿namespace ComputerService.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ComputerService.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    [ObservableProperty] private bool _isSelectedFirstPage = true;
+    [ObservableProperty] private bool _isSelectedSecondPage = false;
+    [ObservableProperty] private bool _isSelectedThirdPage = false;
+    [ObservableProperty] private bool _isSelectedFourthPage = false;
+
+    public void ChangePage(int page)
+    {
+        IsSelectedFirstPage = page == 1;
+        IsSelectedSecondPage = page == 2;
+        IsSelectedThirdPage = page == 3;
+        IsSelectedFourthPage = page == 4;
+    }
+    
+    
+
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,11 +1,28 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using ComputerService.ViewModels;
 
 namespace ComputerService.Views;
 
 public partial class MainWindow : Window
 {
+
+    private MainWindowViewModel? ViewModel => DataContext as MainWindowViewModel;
+
     public MainWindow()
     {
         InitializeComponent();
     }
+
+    private void OnRadioButtonClick(object? sender, RoutedEventArgs e)
+    {
+        if (ViewModel == null) return;
+        if (sender is not RadioButton radioButton) return;
+        ViewModel!.ChangePage(Convert.ToInt32(radioButton.Tag));
+    }
+    
+    
+    
+    
 }
